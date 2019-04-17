e1c48d7c9b7c86127908ee8c9bb6da8d7545dde0
DANIEL CHEN <daniel77chen@hotmail.com>
2019-04-17 13:10:44 -0400

    rename to Recorder

--- Recorder/Properties/AssemblyInfo.cs
+++ Recorder/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApp2")]
+[assembly: AssemblyTitle("Recorder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApp2")]
+[assembly: AssemblyProduct("Recorder")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
