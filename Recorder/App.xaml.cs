420cf17e6fa30ccee6347c4693d31bd5e815ab6c
DANIEL CHEN <daniel77chen@hotmail.com>
2019-04-17 13:05:45 -0400

    websocket + windows event

---
+++ Recorder/App.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Runtime.InteropServices;
+using WebSocketSharp.Server;
+using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace Recorder
+{
+    public class Handler : WebSocketBehavior {}
+
+    public partial class App : Application
+    {
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private bool _isExit;
+
+        WinEventDelegate dele = null;
+        WebSocketServer socket;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+ 
+            _notifyIcon = new System.Windows.Forms.NotifyIcon();
+            _notifyIcon.DoubleClick += (s, args) => ShowMainWindow();
+            _notifyIcon.Icon = Recorder.Properties.Resources.MyIcon;
+            _notifyIcon.Visible = true;
+ 
+            CreateContextMenu();
+            dele = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, dele, 0, 0, WINEVENT_OUTOFCONTEXT);
+            socket = new WebSocketServer("ws://localhost:2012/");
+            socket.AddWebSocketService<Handler>("/");
+            socket.Start();
+        }
+ 
+        private void CreateContextMenu()
+        {
+            _notifyIcon.ContextMenuStrip =
+              new System.Windows.Forms.ContextMenuStrip();
+            //_notifyIcon.ContextMenuStrip.Items.Add("MainWindow...").Click += (s, e) => ShowMainWindow();
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
+        }
+ 
+        private void ExitApplication()
+        {
+            _isExit = true;
+            MainWindow.Close();
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+ 
+        private void ShowMainWindow()
+        {
+            if (MainWindow.IsVisible)
+            {
+                if (MainWindow.WindowState == WindowState.Minimized)
+                {
+                    MainWindow.WindowState = WindowState.Normal;
+                }
+                MainWindow.Activate();
+            }
+            else
+            {
+                MainWindow.Show();
+            }
+        }
+ 
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!_isExit)
+            {
+                e.Cancel = true;
+                MainWindow.Hide(); // A hidden window can be shown again, a closed one not
+            }
+        }
+
+
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            // getFilename
+
+            uint processId;
+            if (GetWindowThreadProcessId(hwnd, out processId) > 0)
+            {
+                String filename = Process.GetProcessById((int)processId).MainModule.FileName;
+                socket.WebSocketServices.Broadcast(filename);
+                Console.WriteLine(Process.GetProcessById((int)processId).MainModule.FileName);
+            }
+            // Log.Text = GetActiveWindowTitle() + "\r\n";
+        }
+    }
+}
